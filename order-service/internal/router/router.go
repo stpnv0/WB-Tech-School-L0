8b586805f9cad533eb161b5d88da44329fe47107
Stpnv0 <dmitrystepanov754@gmail.com>
2025-08-24 21:37:08 +0300

    CREATE: implement router

--- order-service/internal/router/router.go
+++ order-service/internal/router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"order-service/internal/handlers"
+)
+
+func InitRouter(orderHandler *handlers.Handler) *gin.Engine {
+	router := gin.Default()
+
+	order := router.Group("/order")
+	{
+		order.GET("/:order_uid", orderHandler.GetOrderByUID)
+	}
+
+	return router
+}
