2758cb0c0cc8e6e78a108d79f9863d85017218be
Stpnv0 <dmitrystepanov754@gmail.com>
2025-08-25 21:38:29 +0300

    CREATE: created router

--- order-service/internal/router/router.go
+++ order-service/internal/router/router.go
@@ -13,5 +13,8 @@ func InitRouter(orderHandler *handlers.Handler) *gin.Engine {
 		order.GET("/:order_uid", orderHandler.GetOrderByUID)
 	}
 
+	router.Static("/static", "./web/static")
+	router.StaticFile("/", "./web/static/index.html")
+
 	return router
 }
